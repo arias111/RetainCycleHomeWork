3c42c0788265a0be23f545bfcedbd5337bd15d1f
Nail Galiev <nail.galiev@waveaccess.ru>
2021-11-14 17:13:22 +0300

    [init] initialized project

---
+++ RetainCycle/RetainCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetainCycle
+//
+//  Created by user on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
